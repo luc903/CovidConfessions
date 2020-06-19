e6b8bbbbea8499d3b5e5dcb7f312337ff849b79c
awilkie1 <ash@squidsoup.org>
2020-06-19 23:37:21 +0100

    Added a few thing in the header need a favicon to follow

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Covid Confessions</title>
+    <title>Confessions</title>
+    
+    <meta name="author" content="Audacious, Ashley Wilkie, Luc Wybourn Whyte">
+    <meta name="description" content="The anonymous portal will absorb your Confessions. The ritual of confessing helps with your well-being. Share those confessions anonymously and be part of our online global participatory project">
+    <meta property="og:image" content="https://confessions.audacious.org.uk/images/render_room.png">
+    <meta property="og:description" content="The anonymous portal will absorb your Confessions. The ritual of confessing helps with your well-being. Share those confessions anonymously and be part of our online global participatory project">
+    <meta property="og:title" content="Confessions">
+    
     <link href="css/uikit.min.css" rel="stylesheet"/>
     <link href="css/styles.css" rel="stylesheet"/>
     <script src="https://aframe.io/releases/1.0.4/aframe.min.js"></script>
